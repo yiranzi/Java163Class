6a6f5baaed07b66c78b887b911c411da7f44a7aa
yiranzi <570932296@qq.com>
2017-01-10 01:47:41 +0800

    no bug and Delete function

--- MyExercise/src/datastruct/Link.java
+++ MyExercise/src/datastruct/Link.java
@@ -8,16 +8,14 @@ public class Link {
 	//链表类是给人随便用的
 	public Link()
 	{
-		nTail = null;
-		nHead = null;
+		nTail = new Node();
+		nHead = new Node();
 		iLength = 0;
 		bInit = false;
 	}
 	
 	public void AddTail(int iData)
 	{
-		
-
 		if (!bInit)
 		{
 			nTail.nNext = new Node(iData);//将链表尾巴元素指向新的节点.
@@ -46,7 +44,7 @@ public class Link {
 	
 	public void LinkPrint()
 	{
-		Node nNow = nHead;
+		Node nNow = nHead.nNext;
 		while(nNow!=null)
 		{
 			System.out.println(nNow.iData);
@@ -54,15 +52,81 @@ public class Link {
 		}
 		
 	}
+	
+	public boolean Delete(int iCount)
+	{
+		int iType = 1;
+		if(iCount>iLength)
+		{
+			return false;
+		}
+		else if(iCount == iLength)
+		{
+			iType = 2;
+		}
+		else if(iCount == 1)
+		{
+			iType = 0;
+		}
+		switch(iType)
+		{
+			case 0:
+				//nHead.nNext = 
+				
+			case 1:
+				
+		}
+
+	}
+	
+	public Node FindPos( int iPos)
+	{
+		if(iPos>iLength)
+		{
+			return null;
+		}
+		Node nNow = nHead.nNext;
+		int iCount = 1;
+		while(true)
+		{
+			System.out.println(nNow.iData);
+			if(nNow.nNext==null)return nNow;
+			nNow = nNow.nNext;
+			iCount ++;
+		}
+		
+
+	}
+	
+	public void clear()
+	{
+		
+	}
+	
+	public int GetLength()
+	{
+		return iLength;
+	}
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		Link link1 = new Link();
 		//link1.AddHead(10);//我想添加一个节点
 		link1.AddTail(20);
+		link1.AddTail(30);
+		link1.AddTail(40);
 		//link1.Find(10);
 		//link1.Delete(10);
 		link1.LinkPrint();
-		
+		System.out.println(link1.GetLength());
+		Link link2 = new Link();
+		//link1.AddHead(10);//我想添加一个节点
+		link2.AddTail(-20);
+		link2.AddTail(-30);
+		link2.AddTail(-40);
+		//link1.Find(10);
+		//link1.Delete(10);
+		link2.LinkPrint();
+		System.out.println(link2.GetLength());
 	}
 }
 
