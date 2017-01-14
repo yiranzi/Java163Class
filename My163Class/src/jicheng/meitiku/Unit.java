c99d51ce5280b080d129a2b7949a738a766cef1e
yiranzi <570932296@qq.com>
2017-01-14 20:14:44 +0800

    extend ver1 plus

---
+++ My163Class/src/jicheng/meitiku/Unit.java
@@ -0,0 +1,17 @@
+package jicheng.meitiku;
+
+public class Unit {
+	int id;
+	String name;
+	int hp;
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+	
+	public void Print()
+	{
+		
+	}
+
+}
